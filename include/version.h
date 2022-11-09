730ce8d8edb141b490a7a6b36530d00f4f1cc308
JaeHoon Sim <jay.sim@lge.com>
2022-11-09 11:01:39 +0900

    Change version string definition
    Set version v2.12.2
    
    Split version string to MAJOR, MIDDLE, MINOR.
    
    MAJOR will be modified if major features are added or main structure or
    algorithm were changed.
    MIDDLE will be modified if some logic and  are changed.
    MINOR will be modified for bug fixes or code cleaning.
    
    Signed-off-by: JaeHoon Sim <jay.sim@lge.com>

--- include/version.h
+++ include/version.h
@@ -1,8 +1,12 @@
 #ifndef _version_h
 #define _version_h
 
-#define	VERSION			"2.12.1"
-#define VERSION_DATE	"30 April 2022"
+#define VERSION_MAJOR   "2"
+#define VERSION_MIDDLE  "12"
+#define VERSION_MINOR   "2"
+
+#define	VERSION         VERSION_MAJOR "." VERSION_MIDDLE "." VERSION_MINOR
+#define VERSION_DATE	"2022-05-09"
 
 #endif  /* _version_h */
 
