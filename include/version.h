785ff731738fcd929ecb66ed72faa3edb8b9e1af
JaeHoon Sim <jay.sim@lge.com>
2025-03-05 14:32:58 +0900

    fatprogs release 2.14.0
    Signed-off-by: JaeHoon Sim <jay.sim@lge.com>

--- include/version.h
+++ include/version.h
@@ -2,10 +2,10 @@
 #define _version_h
 
 #define VERSION_MAJOR   "2"
-#define VERSION_MIDDLE  "13"
-#define VERSION_MINOR   "3"
+#define VERSION_MIDDLE  "14"
+#define VERSION_MINOR   "0"
 
 #define	VERSION         VERSION_MAJOR "." VERSION_MIDDLE "." VERSION_MINOR
-#define VERSION_DATE	"2025-02-28"
+#define VERSION_DATE	"2025-03-10"
 
 #endif  /* _version_h */
